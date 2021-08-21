a7a16899947d55f8c914b65d57f3d19c331dc558
Elisha Ukpong <ishukpong418@gmail.com>
2021-08-21 02:26:16 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
